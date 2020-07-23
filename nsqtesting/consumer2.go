71282b19c328d10cf023e8907ae7cb7bee171502
Jamal Arif <32341971+jamalarif@users.noreply.github.com>
2020-07-23 12:16:51 -0700

    Create consumer2.go

---
+++ nsqtesting/consumer2.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+    "log"
+    "sync"
+
+    "github.com/nsqio/go-nsq"
+)
+
+func main() {
+    wg := &sync.WaitGroup{}
+    wg.Add(1)
+
+    decodeConfig := nsq.NewConfig()
+    c, err := nsq.NewConsumer("NSQ_Topic_2", "NSQ_Channel_1", decodeConfig)
+    if err != nil {
+        log.Panic("Could not create consumer")
+    }
+    //c.MaxInFlight defaults to 1
+
+    c.AddHandler(nsq.HandlerFunc(func(message *nsq.Message) error {
+        log.Println("NSQ message received:")
+        log.Println(string(message.Body))
+        return nil
+    }))
+
+    err = c.ConnectToNSQLookupd("10.0.10.234:4161")
+    if err != nil {
+        log.Panic("Could not connect")
+    }
+    log.Println("Awaiting messages from NSQ topic \"My NSQ Topic\"...")
+    wg.Wait()
+}
